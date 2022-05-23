a565336f1b174492a17d31aa85fb37d9f1a93d06
chichoon <chichoon.choi@gmail.com>
2022-05-24 00:12:23 +0900

    Feat: 광고 컨테이너에서 데이터 가공하는 부분 렌더링부분에서 위로 올려주기

--- src/routes/ManageAds/AdsContainer/index.tsx
+++ src/routes/ManageAds/AdsContainer/index.tsx
@@ -1,6 +1,6 @@
 import { IAds } from 'types/ads'
 import AdsListBlock from './AdsListBlock'
-import { getCreateDate, getBudget, getPercentage } from './utils'
+import { getCreateDate, getBudget, getPercentage, getAdsTitle } from './utils'
 
 import styles from './adsContainer.module.scss'
 
@@ -9,16 +9,23 @@ interface IProps {
 }
 
 export const AdsContainer = ({ ads }: IProps) => {
+  const title = getAdsTitle(ads.title, ads.adType)
+  const date = getCreateDate(ads.startDate, ads.endDate, ads.status)
+  const budget = getBudget(ads.budget)
+  const convValue = getBudget(ads.report.convValue)
+  const cost = getBudget(ads.report.cost)
+  const roas = getPercentage(ads.report.roas)
+
   return (
     <div className={styles.adsContainerWrapper}>
-      <h2>타이틀</h2>
+      <h2>{title}</h2>
       <ul>
         <AdsListBlock dataKey='상태' dataValue={ads.status === 'active' ? '진행중' : '완료'} />
-        <AdsListBlock dataKey='광고 생성일' dataValue={getCreateDate(ads)} />
-        <AdsListBlock dataKey='일 희망 예산' dataValue={getBudget(ads.budget)} />
-        <AdsListBlock dataKey='광고 수익률' dataValue={getPercentage(ads.report.roas)} />
-        <AdsListBlock dataKey='매출' dataValue={getBudget(ads.report.convValue)} />
-        <AdsListBlock dataKey='광고 비용' dataValue={getBudget(ads.report.cost)} />
+        <AdsListBlock dataKey='광고 생성일' dataValue={date} />
+        <AdsListBlock dataKey='일 희망 예산' dataValue={budget} />
+        <AdsListBlock dataKey='광고 수익률' dataValue={roas} />
+        <AdsListBlock dataKey='매출' dataValue={convValue} />
+        <AdsListBlock dataKey='광고 비용' dataValue={cost} />
       </ul>
       <button type='button'>수정하기</button>
     </div>
