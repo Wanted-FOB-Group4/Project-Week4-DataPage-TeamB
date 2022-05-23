03c78e8b74d1845bb10b0e444c6fad4620a560a9
Eunchae Ko <kec0130@gmail.com>
2022-05-23 23:13:13 +0900

    Feat: Add global style

--- src/routes/Dashboard/index.tsx
+++ src/routes/Dashboard/index.tsx
@@ -1,5 +1,21 @@
 const Dashboard = () => {
-  return <div>Dashboard</div>
+  return (
+    <>
+      <h1 className='title'>대시보드</h1>
+      <h2 className='subtitle'>통합 광고 현황</h2>
+      <div className='container'>
+        Lorem ipsum dolor sit, amet consectetur adipisicing elit. Doloremque iure delectus doloribus commodi consectetur
+        minus optio fugiat corporis quos voluptates, facilis aspernatur inventore rerum impedit quia reprehenderit vitae
+        accusantium similique!
+      </div>
+      <h2 className='subtitle'>매체 현황</h2>
+      <div className='container'>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. At, rerum dolorum maxime blanditiis sunt perferendis
+        illum laborum a facere, consequuntur obcaecati, hic perspiciatis atque exercitationem! Ab hic quos adipisci
+        molestias?
+      </div>
+    </>
+  )
 }
 
 export default Dashboard
