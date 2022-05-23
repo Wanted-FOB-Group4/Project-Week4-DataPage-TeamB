15cd2761ca3e6cb24afee25d65c5fe92b425fbc1
chichoon <chichoon.choi@gmail.com>
2022-05-23 22:51:50 +0900

    Feat: 금액 단위 계산 함수 추가

---
+++ src/routes/ManageAds/AdsContainer/utils/getBudget.ts
@@ -0,0 +1,19 @@
+import addCommaToNumbers from './addCommaToNumbers'
+
+const getBudget = (budget: number) => {
+  if (budget < 1000) return `${budget}원`
+  if (budget < 100000) {
+    if (budget % 1000 > 0) return `${Math.floor(budget / 1000)}천 ${budget % 1000}원`
+    return `${Math.floor(budget / 1000)}천원`
+  }
+  const thousand = Math.floor((budget % 10000) / 1000)
+  const tenThousand = Math.floor(budget / 10000)
+  const rest = budget % 1000
+  const strArr = []
+  if (tenThousand > 0) strArr.push(`${addCommaToNumbers(tenThousand)}}만`)
+  if (thousand > 0) strArr.push(`${thousand}천`)
+  if (rest > 0) strArr.push(rest)
+  return `${strArr.join(' ')}원`
+}
+
+export default getBudget
