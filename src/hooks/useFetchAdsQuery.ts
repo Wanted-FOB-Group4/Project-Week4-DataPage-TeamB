cd7320d17c04fe18ae61dd7964d6aeea32fc1c6b
chichoon <chichoon.choi@gmail.com>
2022-05-24 01:01:23 +0900

    Feat: Query hook 반환타입 지정, 에러바운더리 사용설정

--- src/hooks/useFetchAdsQuery.ts
+++ src/hooks/useFetchAdsQuery.ts
@@ -1,12 +1,14 @@
 import { getAdListData } from 'services'
 import { useQuery } from 'react-query'
+import { IAdData } from 'types/ads'
 
-export const useFetchAdsQuery = () => {
+export const useFetchAdsQuery = (): IAdData => {
   const { data } = useQuery(['#adsData'], getAdListData, {
     refetchOnWindowFocus: false,
     staleTime: 60000,
     cacheTime: Infinity,
     suspense: true,
+    useErrorBoundary: true,
   })
   return data
 }
