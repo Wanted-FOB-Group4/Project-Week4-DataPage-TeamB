3050808b45e24691157c3e27dc9f93480e01f8b8
chichoon <chichoon.choi@gmail.com>
2022-05-24 00:51:38 +0900

    Feat: response 말고 response.data 반환하도록 수정

--- src/services/getAdListData.ts
+++ src/services/getAdListData.ts
@@ -1,4 +1,4 @@
 // wanted_FE_ad-list-data-set.json
 import axios from 'axios'
 
-export const getAdListData = () => axios.get('/wanted_FE_ad-list-data-set.json')
+export const getAdListData = () => axios.get('/wanted_FE_ad-list-data-set.json').then((response) => response.data)
