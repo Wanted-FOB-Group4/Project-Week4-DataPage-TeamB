1a4e6bba09b4a62b0261c9d0b44cb87509d31200
HyeongSeoku <gudtjr3437@naver.com>
2022-05-24 13:25:01 +0900

    Feat : Add types

--- src/types/types.d.ts
+++ src/types/types.d.ts
@@ -1,1 +1,25 @@
 // types
+interface ICardData {
+  cardTitle: string
+  cardValue: number[]
+  cardPrevValue: number[]
+  term: number
+}
+
+interface ITrendData {
+  click: number
+  conv: number
+  convValue: number
+  cost: number
+  cpa: number
+  cpc: number
+  ctr: number
+  cvr: number
+  date: string
+  imp: number
+  roas: number
+}
+
+interface IDate {
+  date: { start: string; end: string }
+}
