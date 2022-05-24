c732a7783ae826d58435e7784ec6c842c1a46511
chichoon <chichoon.choi@gmail.com>
2022-05-25 01:52:47 +0900

    Feat: 라디오버튼 컴포넌트 따로 분리 (너무 길어짐)

---
+++ src/routes/ManageAds/AdsHeader/AdsCreateForm/AdsCreateFormAdTypeLi.tsx
@@ -0,0 +1,42 @@
+import styles from './adsCreateFormLi.module.scss'
+
+interface IProps {
+  value: boolean
+  setValue: React.Dispatch<boolean>
+}
+
+const AD_TYPE_WEB = true
+const AD_TYPE_APP = false
+
+const AdsCreateFormAdTypeLi = ({ value, setValue }: IProps) => {
+  const handleAdTypeWebChange = () => setValue(AD_TYPE_WEB)
+  const handleAdTypeAppChange = () => setValue(AD_TYPE_APP)
+
+  return (
+    <li className={styles.createList}>
+      <label htmlFor='adType'>광고 타입</label>
+      <div className={styles.radioInput}>
+        <input
+          name='adType'
+          id='radioWeb'
+          type='radio'
+          className={styles.radioWeb}
+          checked={value === AD_TYPE_WEB}
+          onChange={handleAdTypeWebChange}
+        />
+        <label htmlFor='radioWeb'>웹 광고</label>
+        <input
+          name='adType'
+          id='radioApp'
+          type='radio'
+          className={styles.radioApp}
+          checked={value === AD_TYPE_APP}
+          onChange={handleAdTypeAppChange}
+        />
+        <label htmlFor='radioApp'>앱 광고</label>
+      </div>
+    </li>
+  )
+}
+
+export default AdsCreateFormAdTypeLi
