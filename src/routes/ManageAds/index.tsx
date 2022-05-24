b6fab17b687b2d008ae327c6564bb5fd2205b567
chichoon <chichoon.choi@gmail.com>
2022-05-24 12:13:02 +0900

    Design: 레이아웃 컨테이너와 스타일 합치기 완료

--- src/routes/ManageAds/index.tsx
+++ src/routes/ManageAds/index.tsx
@@ -2,38 +2,18 @@ import { Suspense } from 'react'
 import AdsContainers from './AdsContainers'
 import AdsHeader from './AdsHeader'
 
-import styles from './manageAds.module.scss'
-
 const ManageAds = () => {
   return (
-<<<<<<< HEAD
     <>
       <h1 className='title'>광고관리</h1>
       <div className='container'>
-        Lorem ipsum dolor sit, amet consectetur adipisicing elit. Doloremque iure delectus doloribus commodi consectetur
-        minus optio fugiat corporis quos voluptates, facilis aspernatur inventore rerum impedit quia reprehenderit vitae
-        accusantium similique!
+        <AdsHeader />
+        <Suspense fallback={<div>Loading...</div>}>
+          <AdsContainers />
+        </Suspense>
       </div>
     </>
-=======
-    <main className={styles.manageAdsWrapper}>
-      <AdsHeader />
-      <Suspense fallback={<div>Loading...</div>}>
-        <AdsContainers />
-      </Suspense>
-    </main>
->>>>>>> 2fa99a8 (Feat: 폴더구조, 태그 구조 재조정)
   )
 }
 
 export default ManageAds
-
-{/* <main className={styles.manageAdsWrapper}>
-      <div className={styles.manageAdsTop}>
-        <div>전체 광고</div>
-        <button type='button'>광고 만들기</button>
-      </div>
-      <Suspense fallback={<div>Loading...</div>}>
-        <AdsContainers />
-      </Suspense>
-    </main> */}
