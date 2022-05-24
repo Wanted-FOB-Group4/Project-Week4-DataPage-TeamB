22d678c02cc8eb407a6a24888fe9c206faaedf1f
chichoon <chichoon.choi@gmail.com>
2022-05-25 02:28:15 +0900

    Feat: 광고 컨테이너 클릭했을 때 테두리 색상 변경

--- src/routes/ManageAds/AdsContainers/AdsContainer.tsx
+++ src/routes/ManageAds/AdsContainers/AdsContainer.tsx
@@ -1,15 +1,21 @@
+import { useState, useRef } from 'react'
+import cx from 'classnames'
+
 import { IAd } from 'types/ads'
-import AdsListBlock from './AdsListBlock'
-import { getCreateDate, getPercentage, getAdsTitle } from './utils'
 import { addUnitToBudget } from 'utils'
+import { getCreateDate, getPercentage, getAdsTitle } from './utils'
+import AdsListBlock from './AdsListBlock'
 
 import styles from './adsContainer.module.scss'
+import { useClickAway } from 'react-use'
 
 interface IProps {
   adData: IAd
 }
 
 const AdsContainer = ({ adData }: IProps) => {
+  const [isFocused, setIsFocused] = useState(false)
+  const containerRef = useRef(null)
   const title = getAdsTitle(adData.title, adData.adType)
   const date = getCreateDate(adData.startDate, adData.endDate, adData.status)
   const budget = addUnitToBudget(adData.budget)
@@ -17,18 +23,29 @@ const AdsContainer = ({ adData }: IProps) => {
   const cost = addUnitToBudget(adData.report.cost)
   const roas = getPercentage(adData.report.roas)
 
+  const handleContainerClick = () => setIsFocused(true)
+  const handleContainerClickAway = () => setIsFocused(false)
+  useClickAway(containerRef, handleContainerClickAway)
+
   return (
-    <li className={styles.adsContainerWrapper}>
-      <h2>{title}</h2>
-      <ul>
-        <AdsListBlock dataKey='상태' dataValue={adData.status === 'active' ? '진행중' : '완료'} />
-        <AdsListBlock dataKey='광고 생성일' dataValue={date} />
-        <AdsListBlock dataKey='일 희망 예산' dataValue={budget} />
-        <AdsListBlock dataKey='광고 수익률' dataValue={roas} />
-        <AdsListBlock dataKey='매출' dataValue={convValue} />
-        <AdsListBlock dataKey='광고 비용' dataValue={cost} />
-      </ul>
-      <button type='button'>수정하기</button>
+    <li>
+      <button
+        className={cx(styles.adsContainerWrapper, { [styles.isContainerFocused]: isFocused })}
+        type='button'
+        onClick={handleContainerClick}
+        ref={containerRef}
+      >
+        <h2>{title}</h2>
+        <ul>
+          <AdsListBlock dataKey='상태' dataValue={adData.status === 'active' ? '진행중' : '완료'} />
+          <AdsListBlock dataKey='광고 생성일' dataValue={date} />
+          <AdsListBlock dataKey='일 희망 예산' dataValue={budget} />
+          <AdsListBlock dataKey='광고 수익률' dataValue={roas} />
+          <AdsListBlock dataKey='매출' dataValue={convValue} />
+          <AdsListBlock dataKey='광고 비용' dataValue={cost} />
+        </ul>
+        <button type='button'>수정하기</button>
+      </button>
     </li>
   )
 }
