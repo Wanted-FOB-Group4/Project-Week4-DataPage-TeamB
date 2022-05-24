3cb3635b355e0a860230d66b7855d8b5ab4faa56
chichoon <chichoon.choi@gmail.com>
2022-05-24 14:02:28 +0900

    Feat: ErrorMessage 컴포넌트 추가

---
+++ src/components/ErrorMessage/index.tsx
@@ -0,0 +1,18 @@
+import { DeadFaceIcon } from 'assets/svgs'
+
+import styles from './errorMessage.module.scss'
+
+interface IProps {
+  error: Error
+}
+
+const ErrorMessage = ({ error }: IProps) => {
+  return (
+    <div className={styles.errorWrapper}>
+      <DeadFaceIcon className={styles.errorIcon} />
+      <h1>{error.message}</h1>
+    </div>
+  )
+}
+
+export default ErrorMessage
