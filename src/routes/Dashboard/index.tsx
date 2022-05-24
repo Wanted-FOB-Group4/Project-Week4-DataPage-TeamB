58411c43fdb09e918b766d1c0ebfcc6af70cd111
yhnb3 <60080270+yhnb3@users.noreply.github.com>
2022-05-24 16:59:03 +0900

    Fix : conflict 해결

--- src/routes/Dashboard/index.tsx
+++ src/routes/Dashboard/index.tsx
@@ -1,12 +1,9 @@
-<<<<<<< HEAD
 import ChartByChannel from './ChartByChannel'
 import ChartByDate from './ChartByDate'
 import SelectButton from './SelectButton'
-=======
 import CardList from './CardList'
 
 const TMP_DATE = { start: '2022-02-06', end: '2022-02-07' }
->>>>>>> 9e59199d0530cce5f5cd25f4a253997248646a32
 
 const Dashboard = () => {
   return (
@@ -14,12 +11,9 @@ const Dashboard = () => {
       <h1 className='title'>대시보드</h1>
       <h2 className='subtitle'>통합 광고 현황</h2>
       <div className='container'>
-<<<<<<< HEAD
+        <CardList date={TMP_DATE} />
         <SelectButton />
         <ChartByDate />
-=======
-        <CardList date={TMP_DATE} />
->>>>>>> 9e59199d0530cce5f5cd25f4a253997248646a32
       </div>
       <h2 className='subtitle'>매체 현황</h2>
       <div className='container'>
