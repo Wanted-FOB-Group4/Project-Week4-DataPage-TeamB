de0f640aa12abba6598ea00b1d22452edc1181e7
chichoon <chichoon.choi@gmail.com>
2022-05-25 04:01:35 +0900

    Feat: 광고 데이터 전역으로 저장하는 atom 추가

---
+++ src/states/adsDataState.ts
@@ -0,0 +1,10 @@
+import { atom } from 'recoil'
+import { IAdData } from 'types/ads'
+
+export const adsDataState = atom<IAdData>({
+  key: '#adsDataState',
+  default: {
+    count: 0,
+    ads: [],
+  },
+})
--- src/states/index.ts
+++ src/states/index.ts
@@ -1,2 +1,3 @@
 export { adsFilterIndexState } from './adsFilterIndexState'
 export { adsCurrentIndexState } from './adsCurrentIndexState'
+export { adsDataState } from './adsDataState'
