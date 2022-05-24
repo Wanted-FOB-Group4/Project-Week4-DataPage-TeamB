f0162466c8100ace7e2030560884bac961128d07
chichoon <chichoon.choi@gmail.com>
2022-05-25 03:40:59 +0900

    Feat: localStorage에서 데이터 가져오는 promise 생성

--- src/routes/ManageAds/services/getAdListData.ts
+++ src/routes/ManageAds/services/getAdListData.ts
@@ -1,21 +1,44 @@
+import { setFetchDelayPromise } from '../../../utils/setFetchDelay'
 import axios, { AxiosResponse } from 'axios'
+import store from 'store'
 
 import { IAd, IAdData } from 'types/ads'
 import { setFetchDelay } from 'utils'
 
 const STATUS = ['all', 'active', 'ended']
 
-export const getAdListData = (adsFilterIndex: number) =>
-  axios
-    .get('/wanted_FE_ad-list-data-set.json')
-    .then(setFetchDelay(500))
-    .then((response: AxiosResponse) => {
-      if (adsFilterIndex === 0) return response.data
-      const newData: IAdData = {
-        count: 0,
-        ads: [],
-      }
-      newData.ads = response.data.ads.filter((ad: IAd) => ad.status === STATUS[adsFilterIndex])
-      newData.count = newData.ads.length
-      return newData
+export const getAdListData = (adsFilterIndex: number) => {
+  const filterData = (data: IAdData) => {
+    if (adsFilterIndex === 0) return data
+    const newData: IAdData = {
+      count: 0,
+      ads: [],
+    }
+    newData.ads = data.ads.filter((ad: IAd) => ad.status === STATUS[adsFilterIndex])
+    newData.count = newData.ads.length
+    return newData
+  }
+
+  const promise = new Promise((resolve: (value: IAdData) => void, reject) => {
+    const data: IAdData = store.get('adsData')
+    if (!data) {
+      console.log('data not found in local storage... fetch from axios')
+      reject()
+    } else resolve(data)
+  })
+  return promise
+    .then(setFetchDelayPromise(500))
+    .then((data: IAdData) => {
+      return filterData(data)
     })
+    .catch(() =>
+      axios
+        .get('/wanted_FE_ad-list-data-set.json')
+        .then(setFetchDelay(500))
+        .then((response: AxiosResponse) => {
+          const newData = filterData(response.data)
+          store.set('adsData', newData)
+          return newData
+        })
+    )
+}
