9bd73d4a751f861eb37b3f662e73e14ed405abc0
HyeongSeoku <gudtjr3437@naver.com>
2022-05-24 18:37:06 +0900

    Feat : get media channel data api

--- src/services/getMediaChannalData.ts
+++ src/services/getMediaChannalData.ts
@@ -1,2 +1,18 @@
 // wanted_FE-media-channel-data-set.json
-export {}
+
+import axios from 'axios'
+import { translateDate } from 'utils/translateDate'
+
+export const getMediaChannelData = (startDate: string, endDate: string) =>
+  axios.get('/wanted_FE-media-channel-data-set.json').then((response) => {
+    const { data } = response
+    const result = data.filter(
+      (item: IMediaChannelData) =>
+        translateDate(item.date) >= translateDate(startDate) && translateDate(item.date) <= translateDate(endDate)
+    )
+    if (result.length === 0) console.log('error')
+
+    console.log(result)
+
+    return result
+  })
