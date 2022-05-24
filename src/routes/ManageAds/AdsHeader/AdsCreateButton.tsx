f422f65b6595ddf4acef0da17176dd7495798c75
chichoon <chichoon.choi@gmail.com>
2022-05-24 16:47:50 +0900

    Feat: 광고 추가 버튼 누르면 하단에 컴포넌트 열기 / 닫기

---
+++ src/routes/ManageAds/AdsHeader/AdsCreateButton.tsx
@@ -0,0 +1,32 @@
+import { useState, useRef } from 'react'
+import { useClickAway } from 'react-use'
+import { cx } from 'styles'
+import styles from './adsCreateButton.module.scss'
+import AdsCreateForm from './AdsCreateForm'
+
+const AdsCreateButton = () => {
+  const [isCreateBoxHidden, setIsCreateBoxHidden] = useState(true)
+  const buttonRef = useRef(null)
+
+  const handleClickAway = () => {
+    setIsCreateBoxHidden(true)
+  }
+
+  const handleButtonClick = () => {
+    setIsCreateBoxHidden((prevState) => !prevState)
+  }
+
+  useClickAway(buttonRef, handleClickAway)
+  return (
+    <div className={styles.createWrapper} ref={buttonRef}>
+      <button type='button' className={styles.createButton} onClick={handleButtonClick}>
+        광고 만들기
+      </button>
+      <div className={cx(styles.createBox, { [styles.isCreateBoxHidden]: isCreateBoxHidden })}>
+        <AdsCreateForm />
+      </div>
+    </div>
+  )
+}
+
+export default AdsCreateButton
