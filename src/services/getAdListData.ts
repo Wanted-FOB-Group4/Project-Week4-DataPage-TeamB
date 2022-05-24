c7b29ce1e33832067f4dcafa214507ab0aa0bedc
chichoon <chichoon.choi@gmail.com>
2022-05-24 13:11:03 +0900

    Feat: 데이터 fetch 시에 고의로 Timeout 주기 성공

--- src/services/getAdListData.ts
+++ src/services/getAdListData.ts
@@ -1,17 +1,28 @@
 import { IAd, IAdData } from 'types/ads'
 // wanted_FE_ad-list-data-set.json
-import axios from 'axios'
+import axios, { AxiosResponse } from 'axios'
 
 const STATUS = ['all', 'active', 'ended']
 
+const delay = () => {
+  return (x: AxiosResponse) => {
+    return new Promise<AxiosResponse>((resolve) => {
+      setTimeout(() => resolve(x), 500)
+    })
+  }
+}
+
 export const getAdListData = (adsFilterIndex: number) =>
-  axios.get('/wanted_FE_ad-list-data-set.json').then((response) => {
-    if (adsFilterIndex === 0) return response.data
-    const newData: IAdData = {
-      count: 0,
-      ads: [],
-    }
-    newData.ads = response.data.ads.filter((ad: IAd) => ad.status === STATUS[adsFilterIndex])
-    newData.count = newData.ads.length
-    return newData
-  })
+  axios
+    .get('/wanted_FE_ad-list-data-set.json')
+    .then(delay())
+    .then((response: AxiosResponse) => {
+      if (adsFilterIndex === 0) return response.data
+      const newData: IAdData = {
+        count: 0,
+        ads: [],
+      }
+      newData.ads = response.data.ads.filter((ad: IAd) => ad.status === STATUS[adsFilterIndex])
+      newData.count = newData.ads.length
+      return newData
+    })
