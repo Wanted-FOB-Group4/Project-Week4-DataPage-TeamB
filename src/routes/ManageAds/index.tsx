76b6b276e38344baeb73eff03589163e192fe7ac
chichoon <chichoon.choi@gmail.com>
2022-05-24 14:04:28 +0900

    Feat: 광고관리 페이지 ErrorBoundary 추가

--- src/routes/ManageAds/index.tsx
+++ src/routes/ManageAds/index.tsx
@@ -3,15 +3,21 @@ import { Suspense } from 'react'
 import Loading from 'components/Loading'
 import AdsContainers from './AdsContainers'
 import AdsHeader from './AdsHeader'
+import { ErrorBoundary } from 'react-error-boundary'
+import ErrorMessage from 'components/ErrorMessage'
 
 const ManageAds = () => {
+  const handleErrorFallback = ({ error }: { error: Error }) => <ErrorMessage error={error} />
+
   return (
     <>
       <h1 className='title'>광고관리</h1>
       <div className='container'>
         <AdsHeader />
         <Suspense fallback={<Loading />}>
-          <AdsContainers />
+          <ErrorBoundary fallbackRender={handleErrorFallback}>
+            <AdsContainers />
+          </ErrorBoundary>
         </Suspense>
       </div>
     </>
