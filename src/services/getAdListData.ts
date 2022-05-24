e452fd0f65081de080e21e18df86904c99b54044
chichoon <chichoon.choi@gmail.com>
2022-05-24 12:36:12 +0900

    Feat: useQuery와 fetch 함수에 필터링 기능 추가

--- src/services/getAdListData.ts
+++ src/services/getAdListData.ts
@@ -1,4 +1,17 @@
+import { IAd, IAdData } from 'types/ads'
 // wanted_FE_ad-list-data-set.json
 import axios from 'axios'
 
-export const getAdListData = () => axios.get('/wanted_FE_ad-list-data-set.json').then((response) => response.data)
+const STATUS = ['all', 'active', 'ended']
+
+export const getAdListData = (adsFilterIndex: number) =>
+  axios.get('/wanted_FE_ad-list-data-set.json').then((response) => {
+    if (adsFilterIndex === 0) return response.data
+    const newData: IAdData = {
+      count: 0,
+      ads: [],
+    }
+    newData.ads = response.data.ads.filter((ad: IAd) => ad.status === STATUS[adsFilterIndex])
+    newData.count = newData.ads.length
+    return newData
+  })
