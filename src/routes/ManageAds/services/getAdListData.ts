26c1bb4fa8263c21887c10c7fd9659f80fefd0a2
chichoon <chichoon.choi@gmail.com>
2022-05-25 04:02:47 +0900

    Feat: 데이터 받아오자마자 필터링 하지 않게 수정

--- src/routes/ManageAds/services/getAdListData.ts
+++ src/routes/ManageAds/services/getAdListData.ts
@@ -1,23 +1,10 @@
 import axios, { AxiosResponse } from 'axios'
 import store from 'store'
 
-import { IAd, IAdData } from 'types/ads'
+import { IAdData } from 'types/ads'
 import { setFetchDelay, setFetchDelayPromise } from 'utils'
 
-const STATUS = ['all', 'active', 'ended']
-
-export const getAdListData = (adsFilterIndex: number) => {
-  const filterData = (data: IAdData) => {
-    if (adsFilterIndex === 0) return data
-    const newData: IAdData = {
-      count: 0,
-      ads: [],
-    }
-    newData.ads = data.ads.filter((ad: IAd) => ad.status === STATUS[adsFilterIndex])
-    newData.count = newData.ads.length
-    return newData
-  }
-
+export const getAdListData = () => {
   const promise = new Promise((resolve: (value: IAdData) => void, reject) => {
     const data: IAdData = store.get('adsData')
     if (!data) {
@@ -27,17 +14,11 @@ export const getAdListData = (adsFilterIndex: number) => {
   })
   return promise
     .then(setFetchDelayPromise(500))
-    .then((data: IAdData) => {
-      return filterData(data)
-    })
+    .then((data: IAdData) => data)
     .catch(() =>
       axios
         .get('/wanted_FE_ad-list-data-set.json')
         .then(setFetchDelay(500))
-        .then((response: AxiosResponse) => {
-          const newData = filterData(response.data)
-          store.set('adsData', newData)
-          return newData
-        })
+        .then((response: AxiosResponse) => response.data)
     )
 }
