b1ed182b93d80dda3e5eb2858ea14e51324cd8aa
chichoon <chichoon.choi@gmail.com>
2022-05-25 04:16:43 +0900

    Feat: useFilterAds 훅에 promise 추가, set 함수 비동기 핸들링

--- src/routes/ManageAds/hooks/useFilterAds.ts
+++ src/routes/ManageAds/hooks/useFilterAds.ts
@@ -1,25 +1,30 @@
+import { adsDataState, adsFilterIndexState } from 'states'
 import { useState, useEffect } from 'react'
 import { useRecoilValue } from 'recoil'
 
-import { adsFilterIndexState } from 'states'
 import { IAd, IAdData } from 'types/ads'
 
 const STATUS = ['all', 'active', 'ended']
 
-export const useFilterAds = (data: IAdData) => {
+export const useFilterAds = () => {
+  const adsData = useRecoilValue(adsDataState)
   const adsFilterIndex = useRecoilValue(adsFilterIndexState)
   const [filteredData, setFilteredData] = useState<IAdData>({ count: 0, ads: [] })
 
   useEffect(() => {
-    if (adsFilterIndex === 0) setFilteredData(data)
-    const newData: IAdData = {
-      count: 0,
-      ads: [],
-    }
-    newData.ads = data.ads.filter((ad: IAd) => ad.status === STATUS[adsFilterIndex])
-    newData.count = newData.ads.length
-    setFilteredData(newData)
-  }, [data, adsFilterIndex])
+    const filter = new Promise<IAdData>((resolve) => {
+      if (adsFilterIndex === 0) resolve(adsData)
+      const newData: IAdData = {
+        count: 0,
+        ads: [],
+      }
+      newData.ads = adsData.ads.filter((ad: IAd) => ad.status === STATUS[adsFilterIndex])
+      newData.count = newData.ads.length
+      resolve(newData)
+    })
+
+    filter.then((newData) => setFilteredData(newData))
+  }, [adsData, adsFilterIndex])
 
   return filteredData
 }
