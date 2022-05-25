4e51a82096b3038d4d23df3bdba07510d4d59306
chichoon <chichoon.choi@gmail.com>
2022-05-25 14:00:12 +0900

    Feat: 모달로 교체 완료

--- src/routes/ManageAds/AdsHeader/AdsCreateButton/index.tsx
+++ src/routes/ManageAds/AdsHeader/AdsCreateButton/index.tsx
@@ -1,29 +1,22 @@
-import { useState, useRef } from 'react'
-import { useClickAway } from 'react-use'
+import { useState } from 'react'
 
-import AdsEditFormContainer from 'routes/ManageAds/_shared/AdsEditFormContainer'
+import AdsEditFormModal from 'routes/ManageAds/_shared/AdsEditFormModal'
 
 import styles from './adsCreateButton.module.scss'
 
 const AdsEditButton = () => {
   const [isCreateBoxHidden, setIsCreateBoxHidden] = useState(true)
-  const buttonRef = useRef(null)
-
-  const handleClickAway = () => {
-    setIsCreateBoxHidden(true)
-  }
 
   const handleButtonClick = () => {
     setIsCreateBoxHidden((prevState) => !prevState)
   }
 
-  useClickAway(buttonRef, handleClickAway)
   return (
-    <div className={styles.createWrapper} ref={buttonRef}>
+    <div className={styles.createWrapper}>
       <button type='button' className={styles.createButton} onClick={handleButtonClick}>
         광고 만들기
       </button>
-      {!isCreateBoxHidden && <AdsEditFormContainer setIsHidden={setIsCreateBoxHidden} />}
+      {!isCreateBoxHidden && <AdsEditFormModal setIsHidden={setIsCreateBoxHidden} />}
     </div>
   )
 }
--- src/routes/ManageAds/_shared/AdsEditFormModal/index.tsx
+++ src/routes/ManageAds/_shared/AdsEditFormModal/index.tsx
@@ -1,4 +1,4 @@
-import { useState, Dispatch, FormEvent, ChangeEvent, SetStateAction } from 'react'
+import { useState, Dispatch, FormEvent, ChangeEvent, SetStateAction, useRef } from 'react'
 import { useRecoilState } from 'recoil'
 import dayjs from 'dayjs'
 import store from 'store'
@@ -8,14 +8,16 @@ import AdsEditFormInput from './AdsEditFormInput'
 import { IAd, IAdData } from 'types/ads'
 import { adsDataState } from 'states'
 
-import styles from './adsEditFormContainer.module.scss'
+import styles from './adsEditFormModal.module.scss'
+import ModalPortal from './ModalPortal'
+import { useClickAway } from 'react-use'
 
 interface IProps {
   prevData?: IAd
   setIsHidden: Dispatch<SetStateAction<boolean>>
 }
 
-const AdsEditFormContainer = ({ prevData, setIsHidden }: IProps) => {
+const AdsEditFormModal = ({ prevData, setIsHidden }: IProps) => {
   const today = dayjs().format('YYYY-MM-DD')
   const [adType, setAdType] = useState(false)
   const [isDone, setIsDone] = useState(false)
@@ -26,6 +28,7 @@ const AdsEditFormContainer = ({ prevData, setIsHidden }: IProps) => {
   const [convValue, setConvValue] = useState(0)
   const [title, setTitle] = useState('')
   const [adsData, setAdsData] = useRecoilState(adsDataState)
+  const modalRef = useRef(null)
 
   const handleFormSubmit = (e: FormEvent<HTMLFormElement>) => {
     e.preventDefault()
@@ -61,31 +64,38 @@ const AdsEditFormContainer = ({ prevData, setIsHidden }: IProps) => {
   const handleBudgetChange = (e: ChangeEvent<HTMLInputElement>) => setBudget(Number(e.currentTarget.value))
   const handleConvValueChange = (e: ChangeEvent<HTMLInputElement>) => setConvValue(Number(e.currentTarget.value))
   const handleCostChange = (e: ChangeEvent<HTMLInputElement>) => setCost(Number(e.currentTarget.value))
+  const handleClickAway = () => setIsHidden(true)
+
+  useClickAway(modalRef, handleClickAway)
 
   return (
-    <div className={styles.createContainer}>
-      <h2>새 광고</h2>
-      <form onSubmit={handleFormSubmit} className={styles.createFormWrapper}>
-        <ul>
-          <AdsEditFormInput name='title' value={title} onChange={handleTitleChange} />
-          <AdsEditFormAdTypeInput value={adType} setValue={setAdType} />
-          <AdsEditFormInput name='isDone' value={isDone} onChange={handleActiveChange} />
-          <AdsEditFormInput name='startDate' value={startDate} onChange={handleStartDateChange} />
-          <AdsEditFormInput
-            name='endDate'
-            value={endDate}
-            onChange={handleEndDateChange}
-            isDone={isDone}
-            startDate={startDate}
-          />
-          <AdsEditFormInput name='budget' value={budget} onChange={handleBudgetChange} />
-          <AdsEditFormInput name='convValue' value={convValue} onChange={handleConvValueChange} />
-          <AdsEditFormInput name='cost' value={cost} onChange={handleCostChange} />
-        </ul>
-        <button type='submit'>추가하기</button>
-      </form>
-    </div>
+    <ModalPortal>
+      <div className={styles.modalBackground}>
+        <div className={styles.editModal} ref={modalRef}>
+          <h2>{prevData ? '광고 수정하기' : '새 광고'}</h2>
+          <form onSubmit={handleFormSubmit} className={styles.editModalFormWrapper}>
+            <ul>
+              <AdsEditFormInput name='title' value={title} onChange={handleTitleChange} />
+              <AdsEditFormAdTypeInput value={adType} setValue={setAdType} />
+              <AdsEditFormInput name='isDone' value={isDone} onChange={handleActiveChange} />
+              <AdsEditFormInput name='startDate' value={startDate} onChange={handleStartDateChange} />
+              <AdsEditFormInput
+                name='endDate'
+                value={endDate}
+                onChange={handleEndDateChange}
+                isDone={isDone}
+                startDate={startDate}
+              />
+              <AdsEditFormInput name='budget' value={budget} onChange={handleBudgetChange} />
+              <AdsEditFormInput name='convValue' value={convValue} onChange={handleConvValueChange} />
+              <AdsEditFormInput name='cost' value={cost} onChange={handleCostChange} />
+            </ul>
+            <button type='submit'>추가하기</button>
+          </form>
+        </div>
+      </div>
+    </ModalPortal>
   )
 }
 
-export default AdsEditFormContainer
+export default AdsEditFormModal
