7739144210616a7477a05ec077574e85babd776c
chichoon <chichoon.choi@gmail.com>
2022-05-25 16:43:07 +0900

    Feat: 화면 폭 좁아지면 스크롤 처리

--- src/routes/ManageAds/AdsHeader/index.tsx
+++ src/routes/ManageAds/AdsHeader/index.tsx
@@ -1,13 +1,24 @@
-import AdsCreateButton from './AdsCreateButton'
+import { useState } from 'react'
+
+import AdsEditFormModal from 'routes/ManageAds/_shared/AdsEditFormModal'
+
 import AdsFilterDropdown from './AdsFilterDropdown'
 
 import styles from './adsHeader.module.scss'
 
 const AdsHeader = () => {
+  const [isCreateBoxHidden, setIsCreateBoxHidden] = useState(true)
+
+  const handleButtonClick = () => {
+    setIsCreateBoxHidden((prevState) => !prevState)
+  }
   return (
     <div className={styles.adsHeaderWrapper}>
       <AdsFilterDropdown />
-      <AdsCreateButton />
+      <button type='button' className={styles.createButton} onClick={handleButtonClick}>
+        광고 만들기
+      </button>
+      {!isCreateBoxHidden && <AdsEditFormModal setIsHidden={setIsCreateBoxHidden} />}
     </div>
   )
 }
