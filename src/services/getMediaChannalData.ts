b039dfe0579727812cb54eb773fb79c141c8b350
HyeongSeoku <gudtjr3437@naver.com>
2022-05-26 05:06:19 +0900

    Feat : Add response type

--- src/services/getMediaChannalData.ts
+++ src/services/getMediaChannalData.ts
@@ -1,6 +1,6 @@
 // wanted_FE-media-channel-data-set.json
 
-import axios from 'axios'
+import axios, { AxiosResponse } from 'axios'
 import { makeChannelData } from 'routes/Dashboard/utils/makeChannelData'
 import { sumChannelData } from 'routes/Dashboard/utils/sumChannedlData'
 import { setFetchDelay } from 'utils'
@@ -10,12 +10,14 @@ export const getMediaChannelData = (startDate: string, endDate: string) =>
   axios
     .get('/wanted_FE-media-channel-data-set.json')
     .then(setFetchDelay(500))
-    .then((response) => {
+    .then((response: AxiosResponse) => {
       const { data } = response
       const result = data.filter(
         (item: IMediaChannelData) =>
           translateDate(item.date) >= translateDate(startDate) && translateDate(item.date) <= translateDate(endDate)
       )
+
       if (result.length === 0) console.log('error')
+
       return sumChannelData(makeChannelData(result))
     })
