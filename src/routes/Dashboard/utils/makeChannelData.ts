94a9bf1c4943b71848d2dc109556507960191473
HyeongSeoku <gudtjr3437@naver.com>
2022-05-26 05:04:59 +0900

    Refactor : Change array to object

--- src/routes/Dashboard/utils/makeChannelData.ts
+++ src/routes/Dashboard/utils/makeChannelData.ts
@@ -1,42 +1,30 @@
 import { IMediaDataItem, IMediaDataByDate } from 'types/chart'
 
+interface InitData {
+  google: IMediaDataByDate[]
+  facebook: IMediaDataByDate[]
+  naver: IMediaDataByDate[]
+  kakao: IMediaDataByDate[]
+
+  [key: string]: any
+}
+
 export const makeChannelData = (apiData: IMediaChannelData[]) => {
-  const google: IMediaDataByDate[] = []
-  const naver: IMediaDataByDate[] = []
-  const kakao: IMediaDataByDate[] = []
-  const facebook: IMediaDataByDate[] = []
+  const newData: InitData = {
+    google: [],
+    facebook: [],
+    kakao: [],
+    naver: [],
+  }
 
   apiData.forEach((item: IMediaDataItem) => {
-    if (item.channel === 'google') {
-      google.push({
-        ...item,
-        conversion: Math.round((item.click * item.cvr) / 100),
-        sales: Math.round((item.roas * item.cost) / 100),
-      })
-      return
-    }
-    if (item.channel === 'naver') {
-      naver.push({
-        ...item,
-        conversion: Math.round((item.click * item.cvr) / 100),
-        sales: Math.round((item.roas * item.cost) / 100),
-      })
-      return
-    }
-    if (item.channel === 'facebook') {
-      facebook.push({
-        ...item,
-        conversion: Math.round((item.click * item.cvr) / 100),
-        sales: Math.round((item.roas * item.cost) / 100),
-      })
-    } else {
-      kakao.push({
-        ...item,
-        conversion: Math.round((item.click * item.cvr) / 100),
-        sales: Math.round((item.roas * item.cost) / 100),
-      })
-    }
+    newData[item.channel].push({
+      ...item,
+      conversion: Math.round((item.click * item.cvr) / 100),
+      sales: Math.round((item.roas * item.cost) / 100),
+    })
   })
+  const { google, kakao, naver, facebook } = newData
 
   return { google, kakao, naver, facebook }
 }
