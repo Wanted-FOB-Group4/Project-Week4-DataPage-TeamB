5cab0947805528bbd183813faa1b19a3718b9f85
HyeongSeoku <gudtjr3437@naver.com>
2022-05-26 05:10:38 +0900

    Fix : Edit logic error

--- src/utils/transformNum.ts
+++ src/utils/transformNum.ts
@@ -1,7 +1,16 @@
 export const transformNum = (NUM: number, TYPE: string) => {
   if (TYPE === 'ROAS') return { unitCnt: Math.floor(NUM), unitWord: '' }
-  const unitCnt = Math.ceil(NUM / 1000)
-  const unit = ['', '천', '만', '억', '조']
-  const unitWord = unit[unitCnt.toString().length]
+  let tmpNum = NUM / 1000
+  let translateNum = Math.floor(tmpNum).toString().length
+
+  if (Math.floor(tmpNum) < 1) {
+    tmpNum *= 1000
+    translateNum = 0
+  }
+
+  const unitCnt = tmpNum
+  const unit = ['', '천', '만', '만', '만', '억', '억', '억', '조']
+  const unitWord = unit[translateNum]
+
   return { unitCnt, unitWord }
 }
