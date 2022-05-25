51ada6b5d5921ccae325d710592bc6355a74dfba
HyeongSeoku <gudtjr3437@naver.com>
2022-05-25 15:36:54 +0900

    Feat : Table component

--- src/routes/Dashboard/Table/TableRow.tsx
+++ src/routes/Dashboard/Table/TableRow.tsx
@@ -1,16 +1,23 @@
+import { useState } from 'react'
+import cx from 'classnames'
+
 import styles from './table.module.scss'
 
-const TableRow = ({ channel, cost, convValue, roas, imp, click, ctr, cpc }: ITable) => {
+const TableRow = ({ rowData, channel }: ITableRow) => {
+  const [isTotalRow] = useState<boolean>(channel === '총합')
+
+  const { cost, sales, roas, imp, click, ctr, cpc } = rowData
+
   return (
-    <tr>
-      <td>{channel}</td>
-      <td>{cost}</td>
-      <td>{roas}</td>
-      <td>{imp}</td>
-      <td>{convValue}</td>
-      <td>{click}</td>
-      <td>{ctr}</td>
-      <td>{cpc}</td>
+    <tr className={cx(styles.tablRowWrapper, { [styles.totalRowWrapper]: isTotalRow })}>
+      <td className={styles.tableTitle}>{channel}</td>
+      <td>{cost.toLocaleString()}</td>
+      <td>{sales.toLocaleString()}</td>
+      <td>{roas.toLocaleString()}</td>
+      <td>{imp.toLocaleString()}</td>
+      <td>{click.toLocaleString()}</td>
+      <td>{ctr.toLocaleString()}</td>
+      <td>{cpc.toLocaleString()}</td>
     </tr>
   )
 }
--- src/routes/Dashboard/Table/index.tsx
+++ src/routes/Dashboard/Table/index.tsx
@@ -2,11 +2,12 @@ import { useQuery } from 'react-query'
 import { useMount } from 'react-use'
 import { getMediaChannelData } from 'services/getMediaChannalData'
 
-import styles from './table.module.scss'
 import TableRow from './TableRow'
 
+import styles from './table.module.scss'
+
 const Table = ({ date }: IDate) => {
-  const { data } = useQuery(['#mediaChannelData', date], () => getMediaChannelData(date.start, date.end), {
+  const { data: tableData } = useQuery(['#mediaChannelData', date], () => getMediaChannelData(date.start, date.end), {
     refetchOnWindowFocus: false,
     staleTime: 60000,
     cacheTime: Infinity,
@@ -15,22 +16,31 @@ const Table = ({ date }: IDate) => {
   })
 
   useMount(() => {
-    console.log(data)
+    console.log(tableData)
   })
 
   return (
     <div>
       <table className={styles.tableContainer}>
-        <tr className={styles.tableHeader}>
-          <th className={styles.tableTitle}> </th>
-          <th>광고비</th>
-          <th>매출</th>
-          <th>ROAS</th>
-          <th>노출수</th>
-          <th>클릭 수</th>
-          <th>클릭률 (CTR)</th>
-          <th>클릭당비용 (CPC)</th>
-        </tr>
+        <thead className={styles.tableHeader}>
+          <tr className={styles.tablRowWrapper}>
+            <th className={styles.tableTitle}> </th>
+            <th>광고비</th>
+            <th>매출</th>
+            <th>ROAS</th>
+            <th>노출수</th>
+            <th>클릭 수</th>
+            <th>클릭률 (CTR)</th>
+            <th>클릭당비용 (CPC)</th>
+          </tr>
+        </thead>
+        <tbody>
+          <TableRow channel='facebook' rowData={tableData!.facebook} />
+          <TableRow channel='naver' rowData={tableData!.naver} />
+          <TableRow channel='google' rowData={tableData!.google} />
+          <TableRow channel='kakao' rowData={tableData!.kakao} />
+          <TableRow channel='총합' rowData={tableData!.total} />
+        </tbody>
       </table>
     </div>
   )
