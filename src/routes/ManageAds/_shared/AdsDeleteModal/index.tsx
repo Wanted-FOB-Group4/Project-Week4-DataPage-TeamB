063117dda0aa1939eb15a8719bcd483a402b7220
chichoon <chichoon.choi@gmail.com>
2022-05-25 22:18:01 +0900

    Feat: 삭제 모달 완성

--- src/routes/ManageAds/_shared/AdsDeleteModal/index.tsx
+++ src/routes/ManageAds/_shared/AdsDeleteModal/index.tsx
@@ -1,13 +1,14 @@
 import { useRef, Dispatch, SetStateAction } from 'react'
+import { useClickAway } from 'react-use'
 import { useRecoilState } from 'recoil'
 import store from 'store'
 
 import { adsDataState } from 'routes/ManageAds/states'
 import ModalPortal from '../ModalPortal'
+import { IAd, IAdData } from 'routes/ManageAds/types'
+import { Button } from 'routes/ManageAds/_shared'
 
 import styles from './adsDeleteModal.module.scss'
-import { IAd, IAdData } from 'routes/ManageAds/types'
-import { useClickAway } from 'react-use'
 
 interface IProps {
   id: number
@@ -43,12 +44,12 @@ const AdsDeleteModal = ({ id, title, setIsHidden }: IProps) => {
           <h2>정말로 해당 광고를 삭제하시겠습니까?</h2>
           <h3>{title}</h3>
           <div className={styles.deleteModalButtons}>
-            <button type='button' onClick={handleConfirmDeleteClick}>
+            <Button type='button' onClick={handleConfirmDeleteClick}>
               예
-            </button>
-            <button type='button' onClick={handleCancelDeleteClick}>
+            </Button>
+            <Button type='button' onClick={handleCancelDeleteClick}>
               아니오
-            </button>
+            </Button>
           </div>
         </div>
       </div>
